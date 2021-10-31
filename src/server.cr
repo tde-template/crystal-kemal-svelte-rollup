7039c26edd6fbc79c733cad57fc5361b5f4e8561
Thibault DESAULES <tdesaules@outlook.com>
2021-10-31 18:48:34 +0100

    first commit

---
+++ src/server.cr
@@ -0,0 +1,16 @@
+#     author: thibault@desaules.me
+#     contributor: none
+#     licence: Apache License, Version 2.0 (https://opensource.org/licenses/Apache-2.0)
+
+require "kemal"
+
+get "/" do |env|
+  send_file(env, "./public/index.html", "text/html")
+end 
+
+ws "/socket" do |socket|
+  socket.send "Hello World!"
+end
+
+Kemal.config.port = 5000
+Kemal.run
